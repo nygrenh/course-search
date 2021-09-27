1d3b4cd9c4560becb61cb48d1d8f77f8060c44c3
Daniel Koch <daniel.koch@helsinki.fi>
2021-09-27 13:53:22 +0300

    Typesense schema and wait for typesense

--- src/app.js
+++ src/app.js
@@ -1,34 +1,39 @@
 const Typesense = require('typesense')
 
-let client = new Typesense.Client({
-    'nodes': [{
-      'host': 'typesense', 
-      'port': '8108',      
-      'protocol': 'http'
-    }],
-    'apiKey': 'xyz',
-    'connectionTimeoutSeconds': 2
-  })
-
-  let courseSchema = {
-    'name': 'courses',
-    'fields': [
-      {'name': 'id', 'type': 'int32' },
-      {'name': 'name', 'type': 'string' },
-      {'name': 'teachers', 'type': 'string[]', 'facet': true },
-      {'name': 'study_level', 'type': 'string' },
-      {'name': 'credits', 'type': 'int32' },
-      {'name': 'course_code', 'type': 'string' },
-      {'name': 'language', 'type': 'string' },
-      {'name': 'type', 'type': 'string' },
-      {'name': 'degree_programme', 'type': 'string' },
-      {'name': 'study_strack', 'type': 'string' }
-    ],
-    'default_sorting_field': 'id'
-  }
-
-  client.collections().create(courseSchema)
-    .then(function (data) {
-      console.log(data)
+async function main () {
+  await sleep(10*1000)
+  let client = new Typesense.Client({
+      'nodes': [{
+        'host': 'typesense', 
+        'port': '8108',      
+        'protocol': 'http'
+      }],
+      'apiKey': 'xyz',
+      'connectionTimeoutSeconds': 10
     })
-  
+
+    let courseSchema = {
+      'name': 'courses',
+      'fields': [
+        {'name': 'name', 'type': 'string' },
+        {'name': 'teachers', 'type': 'string[]', 'facet': true },
+        {'name': 'study_level', 'type': 'string' },
+        {'name': 'credits', 'type': 'int32' },
+        {'name': 'course_code', 'type': 'string' },
+        {'name': 'language', 'type': 'string' },
+        {'name': 'type', 'type': 'string' },
+        {'name': 'degree_programme', 'type': 'string' },
+        {'name': 'study_strack', 'type': 'string' },
+        {'name': 'is_open_university_course', 'type': 'bool'}
+      ],
+    }
+
+    client.collections().create(courseSchema)
+      .then(function (data) {
+        console.log(data)
+      })
+}
+
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+main()
